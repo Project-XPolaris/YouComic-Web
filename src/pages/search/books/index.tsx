a26bf7779595fdf7c89f7873062f89699c6b13c7
allentom <qweasdzxczse2@gmail.com>
2020-03-25 19:20:56 +0900

    修改搜索书籍页面的Pagination

--- src/pages/search/books/index.tsx
+++ src/pages/search/books/index.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import { connect, Dispatch } from 'dva';
 import { ConnectType } from '@/global/connect';
 import useStyles from './style';
 import BookRowCollection from '@/pages/home/components/BookRowCollection';
 import { SearchBooksModelStateType } from '@/pages/search/books/model';
 import { LayoutModelStateType } from '@/models/layout';
-import Pagination from '@/layouts/components/Pagination';
-
+import MaterialPagination from '@material-ui/lab/Pagination';
 
 interface SearchBooksPagePropsType {
   dispatch: Dispatch,
@@ -18,38 +16,27 @@ interface SearchBooksPagePropsType {
 function SearchBooksPage({ dispatch, searchBooks, layout }: SearchBooksPagePropsType) {
   const classes = useStyles();
   const { books } = searchBooks;
-  const onPaginationChange = (page = searchBooks.page, pageSize = searchBooks.pageSize) => {
+  const onPaginationChange = (e:any,page = searchBooks.page) => {
     dispatch({
       type: 'searchBooks/setPage',
       payload: {
         page,
-        pageSize,
+        pageSize:searchBooks.pageSize,
       },
     });
     dispatch({
       type: 'searchBooks/searchBooks',
     });
   };
-  const onNextPage = () => {
-    onPaginationChange(searchBooks.page + 1);
-  };
-  const onPrevious = () => {
-    onPaginationChange(searchBooks.page - 1);
-  };
-  const onSelectPage = (currentPage: number) => {
-    onPaginationChange(currentPage);
-  };
   return (
     <div className={layout.isDrawerOpen ? classes.mainExpand : classes.main}>
       <BookRowCollection title={''} books={books}/>
       <div className={classes.paginationWrap}>
-        <Pagination
-          count={searchBooks.count}
+        <MaterialPagination
           page={searchBooks.page}
-          pageSize={searchBooks.pageSize}
-          onNextPage={onNextPage}
-          onPreviousPage={onPrevious}
-          onSelectPage={onSelectPage}
+          count={Math.ceil(searchBooks.count / searchBooks.pageSize)}
+          color="primary"
+          onChange={onPaginationChange}
         />
       </div>
     </div>
